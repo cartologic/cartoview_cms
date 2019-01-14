ddb08f5961d8f376bdff4e38641a9f3801e803a8
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-14 12:03:26 +0200

    Converted the repo into cartoview app

---
+++ installer.py
@@ -0,0 +1,21 @@
+__author__ = 'cartologic'
+
+info = {
+    "title": "CMS",
+    "description": "Content Management System for Cartoview",
+    "author": 'Cartologic',
+    "home_page": 'http://cartoview.org/apps/cartoview_cms',
+    "help_url": "http://cartoview.org/apps/cartoview_cms/help/",
+    "tags": ['app', 'cartoview', 'wagtail', 'cms'],
+    "licence": 'BSD',
+    "author_website": "http://www.cartoview.org",
+    "single_instance": True
+}
+
+
+def install():
+    pass
+
+
+def uninstall():
+    pass
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+setup(
+    name='cartoview_cms',
+    packages=find_packages(),
+    version='1.5.5',
+    description='Content Management System for Cartoview',
+    long_description=long_description,
+    author='Cartologic',
+    author_email='info@cartologic.com',
+    url='https://github.com/cartologic/cartoview_cms',
+    include_package_data=True,
+    keywords=['cartoview', 'gis', 'geonode', "django", "web mapping", "applications", "apps", "cms", "wagtail"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django :: 1.8",
+        "Topic :: Scientific/Engineering :: GIS"
+    ],
+    license="BSD",
+    install_requires=['cartoview']
+)
