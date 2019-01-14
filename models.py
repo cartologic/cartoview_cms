eff8ee0721c5730ddd6a670fbc85a2c0be61f8ee
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-14 14:17:03 +0200

    Homepage is now available with streamfield

--- models.py
+++ models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailembeds.blocks import EmbedBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 
-# Create your models here.
+
+class HomePage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('email_field', blocks.EmailBlock()),
+        ('integer', blocks.IntegerBlock()),
+        ('float', blocks.FloatBlock()),
+        ('decimal', blocks.DecimalBlock()),
+        ('url', blocks.URLBlock()),
+        ('check_box', blocks.BooleanBlock()),
+        ('date', blocks.DateBlock()),
+        ('time', blocks.TimeBlock()),
+        ('date_time', blocks.DateTimeBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('HTML', blocks.RawHTMLBlock()),
+        ('quote', blocks.BlockQuoteBlock()),
+        ('choice', blocks.ChoiceBlock()),
+        ('page_chooser', blocks.PageChooserBlock()),
+        ('document', DocumentChooserBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ], blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("body", classname="Full"),
+    ]
