0c4da1524d688361db25ae0b0f67d86e9bda536e
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-01-15 02:16:46 -0800

    Every GeoPage now has a corresponding app instance

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.fields import StreamField
@@ -5,6 +6,8 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailembeds.blocks import EmbedBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+
+from cartoview.app_manager.models import AppInstance
 
 
 class GeoPage(Page):
@@ -28,7 +31,21 @@ class GeoPage(Page):
         ('image', ImageChooserBlock()),
         ('embed', EmbedBlock()),
     ], blank=True)
+    app_instance = models.OneToOneField(AppInstance, on_delete=models.PROTECT, null=True, blank=True)
 
     content_panels = Page.content_panels + [
         StreamFieldPanel("body", classname="Full"),
     ]
+
+    def save(self, *args, **kwargs):
+        if self.app_instance is None:
+            app_instance = AppInstance(title=self.title, config=self.title, owner=self.owner)
+            app_instance.save()
+            self.app_instance = app_instance
+        else:
+            app_instance = self.app_instance
+            app_instance.title = self.title
+            app_instance.config = self.title
+            app_instance.owner = self.owner
+            app_instance.save()
+        super(GeoPage, self).save()
