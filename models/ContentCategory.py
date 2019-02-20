ce58c4b79c905d3d3a74ae3b4cfd92c7e36bfcba
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-02-20 10:39:53 +0200

    "Content Category" is now part of the admin panel not snippets

--- models/ContentCategory.py
+++ models/ContentCategory.py
@@ -1,9 +1,7 @@
 from django.db import models
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailsnippets.models import register_snippet
 
 
-@register_snippet
 class ContentCategory(models.Model):
     name = models.CharField(max_length=255)
 
