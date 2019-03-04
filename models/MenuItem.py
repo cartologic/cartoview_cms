d9792c69b74ea166f2cf1f34d7b8ea32edda4de7
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-03-04 14:20:06 +0200

    Menu Item can include a static page as a child

--- models/MenuItem.py
+++ models/MenuItem.py
@@ -2,5 +2,5 @@ from wagtailmenus.models import MenuPage
 
 
 class MenuItem(MenuPage):
-    subpage_types = ['cartoview_cms.MenuItem', 'cartoview_cms.MenuLink']
+    subpage_types = ['cartoview_cms.MenuItem', 'cartoview_cms.MenuLink', 'cartoview_cms.StaticPage']
     show_in_menus_default = True
