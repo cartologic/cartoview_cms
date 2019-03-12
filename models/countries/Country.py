10ebdbbecd08fbec3f0f86dd033704c4f8d3a0c6
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-03-12 15:55:07 +0200

    Makeover to the countries index page

--- models/countries/Country.py
+++ models/countries/Country.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db import models
 from modelcluster.fields import ParentalManyToManyField
 from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel
 from wagtail.wagtailcore import blocks
@@ -7,12 +8,16 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailembeds.blocks import EmbedBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class Country(Page):
     template = 'cartoview_cms/countries/country.html'
     parent_page_types = ['cartoview_cms.CountriesIndex']
     subpage_types = []
+    main_image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True, null=True
+    )
     body = StreamField([
         ('paragraph', blocks.RichTextBlock(classname="full")),
         ('HTML', blocks.RawHTMLBlock()),
@@ -25,6 +30,7 @@ class Country(Page):
     categories = ParentalManyToManyField('cartoview_cms.ContentCategory', blank=True)
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel('main_image'),
         StreamFieldPanel("body", classname="Full"),
         FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
     ]
