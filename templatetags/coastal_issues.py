a736d476ac45fba00eb35aec18cfe7562309401d
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-03-20 17:33:19 +0200

    Code Cleanup

--- templatetags/coastal_issues.py
+++ templatetags/coastal_issues.py
@@ -6,6 +6,8 @@ register = template.Library()
 
 @register.inclusion_tag('cartoview_cms/templatetags/coastal_issues.html')
 def display_issues():
+    coastal_issues = None
     menu_item = MenuItem.objects.filter(slug="coastal-issues").first()
-    coastal_issues = GeoPage.objects.live().descendant_of(menu_item).type(GeoPage)
+    if menu_item:
+        coastal_issues = GeoPage.objects.live().descendant_of(menu_item).type(GeoPage)
     return {'coastal_issues': coastal_issues}
