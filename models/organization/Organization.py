5bad3d9fcf3d9fb9f99019a5e61e986c3443be84
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-03-23 12:00:34 +0200

    Organizations now have a related country

--- models/organization/Organization.py
+++ models/organization/Organization.py
@@ -1,7 +1,9 @@
+from django import forms
 from django.db import models
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from ..countries.Country import Country
 
 
 class Organization(Page):
@@ -12,6 +14,7 @@ class Organization(Page):
         'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True, null=True
     )
     link = models.URLField(max_length=120, blank=True, null=True)
+    related_country = models.ForeignKey(Country, on_delete=models.PROTECT, related_name="related_country", blank=True, null=True)
     main_organization = models.BooleanField(default=False, blank=False)
     organization_title = models.CharField(max_length=120, blank=True)
     organization_description = models.CharField(max_length=500, blank=True)
@@ -20,6 +23,7 @@ class Organization(Page):
     content_panels = Page.content_panels + [
         ImageChooserPanel('thumbnail'),
         FieldPanel('link'),
+        FieldPanel('related_country', widget=forms.Select),
         MultiFieldPanel([
             FieldPanel('main_organization'),
             FieldPanel('organization_title'),
