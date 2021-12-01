65e7238b2a9630c308db7b2753397e84026a62eb
Muhammad Radwan <mohamed.radwan@cartologic.com>
2021-12-01 11:49:00 +0200

    Remove widget_tweaks app as it's now installed by geonode

--- settings.py
+++ settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS += (
     'modelcluster',
 
     'wagtailmenus',
-    'widget_tweaks',
     'wagtailfontawesome',
 )
 
@@ -28,4 +27,4 @@ CARTOVIEW_CMS_CONTEXT_PROCESSORS = ('wagtailmenus.context_processors.wagtailmenu
 TEMPLATES[0]["OPTIONS"]['context_processors'] += CARTOVIEW_CMS_CONTEXT_PROCESSORS
 
 # SITE_NAME declared at cartoview.settings.py
-WAGTAIL_SITE_NAME = SITE_NAME
+WAGTAIL_SITE_NAME = "CMS"
