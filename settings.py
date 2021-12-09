84ecd400de23c777042b256d2be12b5e62d29793
Muhammad Radwan <mohamed.radwan@cartologic.com>
2021-12-09 19:07:34 +0200

    Install CodeRed CMS

--- settings.py
+++ settings.py
@@ -1,8 +1,20 @@
+PROJECT_DIR = CARTOVIEW_DIR
+
 MIDDLEWARE += (
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 )
 
 INSTALLED_APPS += (
+    # CodeRed CMS
+    'coderedcms',
+    'bootstrap4',
+    'modelcluster',
+    'wagtailmenus',
+    'wagtailfontawesome',
+    'wagtailcache',
+    'wagtailimportexport',
+
+    # Wagtail
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -12,14 +24,11 @@ INSTALLED_APPS += (
     'wagtail.documents',
     'wagtail.images',
     'wagtail.search',
-    'wagtail.admin',
     'wagtail.core',
+    'wagtail.contrib.settings',
     'wagtail.contrib.modeladmin',
-
-    'modelcluster',
-
-    'wagtailmenus',
-    'wagtailfontawesome',
+    'wagtail.contrib.table_block',
+    'wagtail.admin',
 )
 
 CARTOVIEW_CMS_CONTEXT_PROCESSORS = ('wagtailmenus.context_processors.wagtailmenus',)
@@ -28,3 +37,15 @@ TEMPLATES[0]["OPTIONS"]['context_processors'] += CARTOVIEW_CMS_CONTEXT_PROCESSOR
 
 # SITE_NAME declared at cartoview.settings.py
 WAGTAIL_SITE_NAME = "CMS"
+
+# Bootstrap
+BOOTSTRAP4 = {
+    # set to blank since coderedcms already loads jquery and bootstrap
+    'jquery_url': '',
+    'base_url': '',
+    # remove green highlight on inputs
+    'success_css_class': ''
+}
+
+# Tags
+TAGGIT_CASE_INSENSITIVE = True
